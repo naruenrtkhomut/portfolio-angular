727b90d2951d649a260cee9bcb915777ed470146
Naruenart Khomut <naruenartkhomut@hotmail.com>
2024-08-12 03:37:48 +0700

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PageIndexComponent } from './page-index/page-index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: PageIndexComponent, title: 'Naruenat.K'}
+];
